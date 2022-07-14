19197d657caf97298fb232f69f68994f6816470c
karthikeyan <kkalimuthu@xemplarinsights.com>
2022-07-14 15:17:10 +0530

    commited for bidmanagement page added.

--- src/app/views/bid-management/bid-management.component.ts
+++ src/app/views/bid-management/bid-management.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { config_url } from '../shared/constant';
 
 
 @Component({
@@ -11,11 +13,16 @@ export class BidmanagementComponent implements OnInit {
   bid_form: FormGroup | any;
   submitted = false;
   bid_form_show=true;
+  bidstatus:any;
+  contractVehicle:any;
+  aside_type_list:any;
+  opportunity_type_list:any;
+  contract_officer_list:any;
   stepname="Details"
   a="1";
   b="2";
   c="3";
-
+  collapsing = true;
  
 
   personalDetails!: FormGroup;
@@ -25,7 +32,7 @@ export class BidmanagementComponent implements OnInit {
   address_step = false;
   education_step = false;
   step = 1;
-  constructor(private frmbuilder: FormBuilder) { }
+  constructor(private frmbuilder: FormBuilder,private http: HttpClient) { }
 
   ngOnInit(): void {
     this.personalDetails = this.frmbuilder.group({
@@ -56,6 +63,16 @@ export class BidmanagementComponent implements OnInit {
       co_officer_phone:['', [Validators.required]],
       co_officer_email:['', [Validators.required]],
       no_of_response:['', [Validators.required]],
+      
+
+      fund_source:['', [Validators.required]],
+      contract_vehicle:['', [Validators.required]],
+      budget_amount:['', [Validators.required]],
+      posted_date:['', [Validators.required]],
+      question_due_date:['', [Validators.required]],
+      response_due_date:['', [Validators.required]],
+      dbe_goal:['', [Validators.required]],
+
 
       //contact details
        FirstName: ['', [Validators.required]],
@@ -79,6 +96,11 @@ export class BidmanagementComponent implements OnInit {
        UserStatusId:['N'],
       
       });
+      this.GetBidStatus();
+      this.GetContract_vehicle();
+      this.GetSet_aside();
+      this.GetOpportunity_type();
+      this.GetContract_officer();
   }
   get personal() { return this.personalDetails.controls; }
   get education() { return this.educationalDetails.controls; }
@@ -86,7 +108,108 @@ export class BidmanagementComponent implements OnInit {
   get bfc() {
     return this.bid_form.controls;
   }
+  GetBidStatus(){
+    try
+    {
+      this.http.get(config_url+'/app/BidStatus').subscribe(
+        (data: any) => {
+          var response= data.data.BidStatus;
+          this.bidstatus = response;
+            console.log(this.bidstatus)
+        });
+    }
+    catch(e) 
+    {
+      console.log(e); 
+    }
+      
+  }
+  GetContract_vehicle(){
+    try
+    {
+      this.http.get(config_url+'/app/ContractVehicle').subscribe(
+        (data: any) => {
+          var response= data.data.ContractVehicle;
+          this.contractVehicle = response;
+            console.log(this.contractVehicle)
+        });
+    }
+    catch(e) 
+    {
+      console.log(e); 
+    }
+      
+  }
+  GetOpportunity_type(){
+    try
+    {
+      this.http.get(config_url+'/app/Opportunity').subscribe(
+        (data: any) => {
+          var response= data.data.Opportunity;
+          this.opportunity_type_list = response;
+            console.log(this.opportunity_type_list)
+        });
+    }
+    catch(e) 
+    {
+      console.log(e); 
+    }
+      
+  }
+  GetSet_aside(){
+    try
+    {
+      this.http.get(config_url+'/app/SetAsideType').subscribe(
+        (data: any) => {
+          var response= data.data.SetAsideType;
+          this.aside_type_list = response;
+            console.log(this.aside_type_list)
+        });
+    }
+    catch(e) 
+    {
+      console.log(e); 
+    }
+      
+  }
+  GetContract_officer(){
+    try
+    {
+      this.http.get(config_url+'/app/ContractingofficerByJobtitle').subscribe(
+        (data: any) => {
+          var response= data.data.contractingofficer;
+          this.contract_officer_list = response;
+            console.log(this.contract_officer_list)
+        });
+    }
+    catch(e) 
+    {
+      console.log(e); 
+    }
+      
+  }
+  generalsave() 
+  {
+     
+      this.submitted = true;
+      let usertype_id = (<HTMLInputElement>document.getElementById("usertype_id")).value;
+      if(usertype_id == "EMPLOY")
+      {
 
+        if (this.bid_form.invalid) 
+        {
+            return;
+        }
+        else
+        {
+          
+         
+          alert(JSON.stringify(this.bid_form.value))
+       
+        }
+   
+      }
+  }
   next(){
     if(this.step==1){
           this.personal_step = true;
