a41f89ad0ce77df93dfe2b7d25008b1d215bf849
karthikeyan <kkalimuthu@xemplarinsights.com>
2022-07-19 13:34:20 +0530

    commited for emp dashboard page

---
+++ src/app/views/emp-dashboard/emp-dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'emp-dashboard',
+  templateUrl: './emp-dashboard.component.html',
+  styleUrls: ['./emp-dashboard.component.scss']
+})
+export class EmpDashboardComponent implements OnInit {
+
+  dtOptions: DataTables.Settings = {};
+  title = 'datatables';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true
+    };
+  }
+
+  displayStyle = "none";
+
+  openPopup() {
+    this.displayStyle = "block";
+  }
+
+  closePopup(){
+    this.displayStyle = "none";
+  }
+
+}
