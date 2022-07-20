e419382e2bd3fbdf092d543dcb1151d988c47d42
arulselvi <selvim@kainosinnovative.com>
2022-07-20 22:44:37 +0530

    commit integrated Employee Dashboard by Arulselvi

---
+++ src/app/views/emp-dashboard/emp-dashboard.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  AvatarModule,
+  ButtonGroupModule,
+  ButtonModule,
+  CardModule,
+  FormModule,
+  GridModule,
+  NavModule,
+  ProgressModule,
+  TableModule,
+  TabsModule
+} from '@coreui/angular';
+import { IconModule } from '@coreui/icons-angular';
+import { ChartjsModule } from '@coreui/angular-chartjs';
+
+import { EmpDashboardRoutingModule } from './emp-dashboard-routing.module';
+import { EmpDashboardComponent } from './emp-dashboard.component';
+import {DataTablesModule} from 'angular-datatables';
+import { AccordionModule } from "../../lib/accordion/accordion.module";
+
+@NgModule({
+  imports: [
+    EmpDashboardRoutingModule,
+    CardModule,
+    NavModule,
+    IconModule,
+    TabsModule,
+    CommonModule,
+    GridModule,
+    ProgressModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    FormModule,
+    ButtonModule,
+    ButtonGroupModule,
+    ChartjsModule,
+    AvatarModule,
+    AccordionModule,
+    TableModule,
+    DataTablesModule,
+    // WidgetsModule
+  ],
+  declarations: [EmpDashboardComponent]
+})
+export class EmpdashboardModule { }
