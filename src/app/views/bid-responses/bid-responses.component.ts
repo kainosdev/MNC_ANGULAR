71d34d91acaf803d56cb77c9e0fcb6690c545902
arulselvi <selvim@kainosinnovative.com>
2022-07-29 12:47:04 +0530

    Bid Responce by Arulselvi

--- src/app/views/bid-responses/bid-responses.component.ts
+++ src/app/views/bid-responses/bid-responses.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { config_url } from '../shared/constant';
+import { ViewChild, ElementRef } from "@angular/core";
+
 
 @Component({
   selector: 'app-bid-responses',
@@ -6,13 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bid-responses.component.scss']
 })
 export class BidResponsesComponent implements OnInit {
+  [x: string]: any;
+  @ViewChild("fileDropRef", { static: false }) fileDropEl: ElementRef;
+  bid_form: FormGroup | any;
 
-  dtOptions: DataTables.Settings = {};
+  //dtOptions: DataTables.Settings = {};
   title = 'datatables';
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.bid_form = this.frmbuilder.group({
+      social_no:['',[Validators.required]],
+      bidstatus: ['',[Validators.required]],
+      
+    })
 
     this.dtOptions = {
       pagingType: 'full_numbers',
